8daab4c220cd0a21900b8afb7c1876d2842ffbbe
Corentin <corentin.chignin@hotmail.fr>
2019-12-11 22:56:40 +0100

    ajout de particules

--- HyperCasual/Assets/Scripts/DestroyPlants.cs
+++ HyperCasual/Assets/Scripts/DestroyPlants.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class DestroyPlants : MonoBehaviour
 {
-
-    void Start()
-    {
-    }
+    public GameObject particules;
     private void OnTriggerEnter(Collider other)
     {
         GenerateBle g = FindObjectOfType<GenerateBle>();
         g.nbDetruis++;
+        
+        GameObject part = Instantiate(particules,gameObject.transform.position, other.transform.rotation);
         Destroy(this.gameObject);
     }
 }
--- HyperCasual/Assets/Scripts/csDestroyEffect.cs
+++ HyperCasual/Assets/Scripts/csDestroyEffect.cs
@@ -5,7 +5,7 @@ public class csDestroyEffect : MonoBehaviour {
     private float count = 0;
 	void Update () {
         count += Time.deltaTime;
-        if (count >= 2f)
+        if (count >= 0.9f)
         {
             Destroy(gameObject);
         }
